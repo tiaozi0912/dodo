ea90e36429e536b24380ce18b98de7fa9e8551ab
Yujun Wu <wyj0912@gmail.com>
2013-08-29 13:35:25 -0700

    need to implement scrollspy

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,25 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   include EventsHelper
-
-  helper_method :formatted_date
-  helper_method :current_user, :me?
+  include ApplicationHelper
 
   def participants_to_s e
   	e.participants_to_s
-  end
-
-  def formatted_date dt    
-  	diff = ((dt - Post.base_day) / 24 / 3600).ceil # should round up
-  	"#{diff} day. #{dt.localtime.strftime('%l:%M %P')}"  # 9 day.9:00 PM
-  end
-
-  def current_user
-  	session[:current_user]
-  end
-
-  def me?
-  	current_user == "Yujun"
   end
 end
 
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,21 @@
 module ApplicationHelper
+	def formatted_date dt
+  	"#{diff_in_days(dt)} day. #{time_str(dt)}"
+  end
+
+  def diff_in_days dt
+    ((dt - Post.base_day) / 24 / 3600).ceil # should round up
+  end
+
+  def time_str dt # 9 day.9:00 PM
+    dt.localtime.strftime('%l:%M %P') 
+  end
+
+  def current_user
+  	session[:current_user]
+  end
+
+  def me?
+  	current_user == "Yujun"
+  end
 end
